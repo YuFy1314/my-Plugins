6c2faacd853a491107577c3a811b034c4daf321b
yuanfei <yufy_info@qq.com>
2018-10-18 12:12:59 +0800

    update

--- 选项卡/js/yufy-tab.js
+++ 选项卡/js/yufy-tab.js
@@ -1,17 +1,3 @@
-/**
- * * 选项卡插件
- * * author: @苏日俪格
- * @param {object} $el 容器(必填)
- * @param {object} tabBtn 选项的元素(必填)
- * @param {object} tabDiv 选项内容的元素(必填)
- * @param {object} tabEve 选项事件(默认onclick)
- * @param {object} tabBtnStyle //选项的样式
- * @param {object} tabDivStyle //选项内容的样式
- * @param {object} tabBtnHoverStyle //选项hover的样式
- * 调用方法
- *     new getTabBar();
- */
-
 (function(win, doc) {
     function tabBar(options) {
         let tabBarConfig = {
